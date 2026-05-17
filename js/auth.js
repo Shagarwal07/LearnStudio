c004aa540a13094f9beaf412b38d45ace2ff6323
Shubham Agarwal <shubham.23bcon0370@jecrcu.edu.in>
2026-05-17 14:24:16 +0530

    resolve ai  issue

--- js/auth.js
+++ js/auth.js
@@ -1,7 +1,9 @@
 function requireAuth() {
-    const token = localStorage.getItem("lms_token");
+    const token = localStorage.getItem("lms_token") || localStorage.getItem("token");
 
     if (!token) {
         window.location.href = "login.html";
     }
 }
+
+window.requireAuth = requireAuth;
